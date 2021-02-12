fcc1a7c62eab26719e585576e5754579995093a2
icefirebear <woungsub1234@gmail.com>
2021-02-13 00:23:08 +0900

    dstagram: 어드민페이지 커스텀 PhotoAdmin 생성

--- dstagram/config/settings.py
+++ dstagram/config/settings.py
@@ -119,3 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = "/static/"
+
+
+import os
+
+MEDIA_URL = "/media/"
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
