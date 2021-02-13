19ad05e308d1033384d024d9ccbc428ed7cd099f
icefirebear <woungsub1234@gmail.com>
2021-02-13 13:07:04 +0900

    dstagram: accounts 앱 url root url연결

--- dstagram/config/urls.py
+++ dstagram/config/urls.py
@@ -18,5 +18,9 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
-urlpatterns = [path("admin/", admin.site.urls), path("", include("photo.urls"))]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include("photo.urls")),
+    path("accounts/", include("accounts.urls")),
+]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
