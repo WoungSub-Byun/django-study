4fc479832556163ef38834d06a0666ad4c8d7317
icefirebear <woungsub1234@gmail.com>
2021-02-14 17:32:34 +0900

    onlineshop: product 상세 view 추가

--- onlineshop/shop/views.py
+++ onlineshop/shop/views.py
@@ -21,3 +21,8 @@ def product_in_category(request, category_slug=None):
             "products": products,
         },
     )
+
+
+def product_detail(request, id, product_slug=None):
+    product = get_object_or_404(Product, id=id, slug=product_slug)
+    return render(request, "shop/detail.html", {"product": product})
