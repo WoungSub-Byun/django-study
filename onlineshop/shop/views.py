278c037b43ea61dbb26b9ded2815bc6443975544
icefirebear <woungsub1234@gmail.com>
2021-02-14 17:30:59 +0900

    onlineshop: category페이지 view 추가

--- onlineshop/shop/views.py
+++ onlineshop/shop/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import *
 
 # Create your views here.
+
+
+def product_in_category(request, category_slug=None):
+    current_category = None
+    categories = Category.objects.all()
+    products = Product.objects.filter(available_display=True)
+
+    if category_slug:
+        current_category = get_object_or_404(Category, slug=categories)
+        products = products.filter(category=current_category)
+    return render(
+        request,
+        "shop/list.html",
+        {
+            "curent_category": current_category,
+            "categories": categories,
+            "products": products,
+        },
+    )
