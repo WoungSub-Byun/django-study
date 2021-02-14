9dc01ab55d8e5778440e26e5d72b46b45a7b866d
icefirebear <woungsub1234@gmail.com>
2021-02-14 17:57:07 +0900

    onlineshop: ProductAdmin 추가

--- onlineshop/shop/admin.py
+++ onlineshop/shop/admin.py
@@ -8,3 +8,23 @@ class CategoryAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Category, CategoryAdmin)
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = [
+        "name",
+        "slug",
+        "category",
+        "price",
+        "stock",
+        "available_display",
+        "available_order",
+        "created",
+        "updated",
+    ]
+    list_filter = ["available_display", "created", "updated", "category"]
+    prepopulated_fields = {"slug": ("name",)}
+    list_editable = ["price", "stock", "available_display", "available_order"]
+
+
+admin.site.register(Product, ProductAdmin)
