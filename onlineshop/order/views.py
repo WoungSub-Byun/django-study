5d7856a9a45ceb7a188b402c8eeea43a6dd58a40
icefirebear <woungsub1234@gmail.com>
2021-02-17 00:00:23 +0900

    onlineshop: OrderCreateAjaxView 추가

--- onlineshop/order/views.py
+++ onlineshop/order/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, get_object_or_404
+from django.views.generic.base import View
+from django.http import JsonResponse
 
 from .models import *
 from cart.cart import Cart
@@ -35,3 +37,25 @@ def order_complete(request):
     order_id = request.GET.get("order_id")
     order = Order.objects.get(id=order_id)
     return render(request, "order/created.html", {"order": order})
+
+class OrderCreateAjaxView(View):
+    def post(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return JsonResponse({"authenticated": False}, status=403)
+        cart = Cart(request)
+        form = OrderCreateForm(request.POST)
+        if form.is_valid():
+            order = form.save(commit=False)
+            if cart.coupon:
+                order.coupon = cart.coupon
+                order.discount = cart.coupon.amount
+            order = form.save()
+            for item in cart:
+                OrderItem.objects.create(order=order, product=item['product'], price=item['price'], quantity=item['quantity'])
+            cart.clear()
+            data = {
+                "order_id": order.id
+            }
+            return JsonResponse(data)
+        else:
+            return JsonResponse({}, status=401)
