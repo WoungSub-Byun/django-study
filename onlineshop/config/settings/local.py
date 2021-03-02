d0100baa74ab0f79d8df54e4d4e863c9da4c5aec
icefirebear <woungsub1234@gmail.com>
2021-03-02 22:40:49 +0900

    onlineshop: update settings file

--- onlineshop/config/settings/local.py
+++ onlineshop/config/settings/local.py
@@ -19,6 +19,6 @@ JWT_AUTH = {
     "JWT_ALGORITHM": config_secret_file["jwt"]["algorithm"],
 }
 # ========================
-ALLOWED_HOSTS = ["*"]
-
+ALLOWED_HOSTS = [".compute.amazonaws.com"]
+DEBUG = False
 DATABASES = config_secret_file["django"]["database"]  # DB 설정
