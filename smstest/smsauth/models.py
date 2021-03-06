24c84e8f3752f578e5621272b74ccbab5a714302
icefirebear <woungsub1234@gmail.com>
2021-03-06 19:19:45 +0900

    smstest: SmsAuth 모델 정의

--- smstest/smsauth/models.py
+++ smstest/smsauth/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class SmsAuth(models.Model):
+    send_to = models.CharField(verbose_name="send_to", max_length=11)
+    auth_code = models.CharField(verbose_name="auth_code", max_length=6)
+    created_at = models.DateTimeField(verbose_name="created_at", auto_now=True)
+
+    class Meta:
+        db_table = "SMSAUTH"
