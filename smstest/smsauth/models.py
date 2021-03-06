ce0817f21abfa400597c7e339cb2bb73ba7f2ae7
icefirebear <woungsub1234@gmail.com>
2021-03-06 22:33:30 +0900

    smstest: 모델 메서드 오류 수정

--- smstest/smsauth/models.py
+++ smstest/smsauth/models.py
@@ -7,7 +7,8 @@ import base64
 import requests
 import time
 import json
-import django.conf import settings
+from django.conf import settings
+
 # Create your models here.
 class SmsAuth(models.Model):
     send_to = models.CharField(verbose_name="send_to", max_length=11)
@@ -32,30 +33,32 @@ class SmsAuth(models.Model):
             return True
         return False
 
-    def	make_signature(self, string):
-        secret_key = bytes(settings.API_SECRET_KEY, 'UTF-8')			
-        signingKey = base64.b64encode(hmac.new(secret_key, string, digestmod=hashlib.sha256).digest())
+    def make_signature(self, string):
+        secret_key = bytes(settings.API_SECRET_KEY, "UTF-8")
+        signingKey = base64.b64encode(
+            hmac.new(secret_key, string, digestmod=hashlib.sha256).digest()
+        )
         return signingKey
 
     def send_sms(self):
         timestamp = str(int(time.time() * 1000))
         uri = "sms/v2/services/{}/messages".format(settings.SMS_SERVICE_ID)
         message = "POST " + uri + "\n" + timestamp + "\n" + settings.API_ACCESS_KEY
-        message = bytes(message, 'UTF-8')
+        message = bytes(message, "UTF-8")
 
-        signature = self.make_signature()
+        signature = self.make_signature(message)
 
         headers = {
             "Content-type": "application/json; charset=UTF-8",
             "x-ncp-apigw-timestamp": timestamp,
             "x-ncp-iam-access-key": settings.API_ACCESS_KEY,
-            "x-ncp-apigw-signature-v2": signature
+            "x-ncp-apigw-signature-v2": signature,
         }
         body = {
             "type": "SMS",
             "contentType": "COMM",
             "from": settings.SMS_SEND_PHONE_NUMBER,
-            "content": "[django-test] 인증 번호 [{}]를 입력해주세요".format(self.auth_code)
-            "messages": [{'to':self.send_to}]
+            "content": "[django-test] 인증 번호 [{}]를 입력해주세요".format(self.auth_code),
+            "messages": [{"to": self.send_to}],
         }
         requests.post(settings.API_URL, data=json.dumps(body), headers=headers)
