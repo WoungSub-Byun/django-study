4a41e634f54f1c6b5cd82ce5dd6e5661b24a42ed
icefirebear <woungsub1234@gmail.com>
2021-03-06 20:49:07 +0900

    smstest: smsauth 모델 메서드 추가

--- smstest/smsauth/models.py
+++ smstest/smsauth/models.py
@@ -1,5 +1,13 @@
 from django.db import models
-
+import sys
+import os
+import hashlib
+import hmac
+import base64
+import requests
+import time
+import json
+import django.conf import settings
 # Create your models here.
 class SmsAuth(models.Model):
     send_to = models.CharField(verbose_name="send_to", max_length=11)
@@ -17,3 +25,37 @@ class SmsAuth(models.Model):
         ).seconds > 180:  # datetime연산은 timedelta 값으로 반환, 인증만료시간 3분
             return True
         return False
+
+    def check_auth_number(self, phone_number, auth_code):
+        obj = self.objects.get(phone_number=phone_number, auth_code=auth_code)
+        if obj.exists():
+            return True
+        return False
+
+    def	make_signature(self, string):
+        secret_key = bytes(settings.API_SECRET_KEY, 'UTF-8')			
+        signingKey = base64.b64encode(hmac.new(secret_key, string, digestmod=hashlib.sha256).digest())
+        return signingKey
+
+    def send_sms(self):
+        timestamp = str(int(time.time() * 1000))
+        uri = "sms/v2/services/{}/messages".format(settings.SMS_SERVICE_ID)
+        message = "POST " + uri + "\n" + timestamp + "\n" + settings.API_ACCESS_KEY
+        message = bytes(message, 'UTF-8')
+
+        signature = self.make_signature()
+
+        headers = {
+            "Content-type": "application/json; charset=UTF-8",
+            "x-ncp-apigw-timestamp": timestamp,
+            "x-ncp-iam-access-key": settings.API_ACCESS_KEY,
+            "x-ncp-apigw-signature-v2": signature
+        }
+        body = {
+            "type": "SMS",
+            "contentType": "COMM",
+            "from": settings.SMS_SEND_PHONE_NUMBER,
+            "content": "[django-test] 인증 번호 [{}]를 입력해주세요".format(self.auth_code)
+            "messages": [{'to':self.send_to}]
+        }
+        requests.post(settings.API_URL, data=json.dumps(body), headers=headers)
